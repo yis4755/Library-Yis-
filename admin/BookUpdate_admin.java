6798277fa6a4629f9211f7c546ac33385b8d9d1a
yis4755 <45478214+yis4755@users.noreply.github.com>
2018-12-19 08:46:46 +0900

    Delete BookUpdate_admin.java

--- admin/BookUpdate_admin.java
+++
@@ -1,9 +0,0 @@
-package admin;
-public class BookUpdate_admin{
-	public BookUpdate_admin() {
-		BookRegister_admin a = new BookRegister_admin();
-		a.setTitle("도서 수정(관리자)");
-		a.button.setText("도서 수정하기");
-	}
-	
-}
